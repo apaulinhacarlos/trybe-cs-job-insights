b6d23eefe14a83f639b86422f0c9a68012f61864
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2022-02-17 18:35:12 -0300

    resolve requisito 3

--- src/insights.py
+++ src/insights.py
@@ -26,21 +26,10 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    data = read(path)
+    unique_industries = set(item["industry"] for item in data)
+    result = (item for item in unique_industries if len(item) != 0)
+    return list(result)
 
 
 def filter_by_industry(jobs, industry):
